12bfbf8439ab0995c36fde0fb859593c8543253e
joopitz <jonas.opitz@live.de>
2023-12-07 14:27:50 +0100

    docs(webservice): add documentation to the different endpoints

--- its_jointprobability/webservice.py
+++ its_jointprobability/webservice.py
@@ -43,12 +43,14 @@ def main():
     data_dir = Path.cwd() / "data"
     model, dictionary, disciplines = import_data(data_dir)
 
+    # collect the possible discipline values in an Enum
     Disciplines_Enum = Enum(
         "Disciplines_Enum",
         dict((discipline, discipline) for discipline in disciplines),
         type=str,
     )
 
+    # classes that define interfaces for the API
     class Discipline(BaseModel):
         name: Disciplines_Enum
         mean_prob: float
@@ -170,9 +172,120 @@ def main():
         def app(self) -> FastAPI:
             app = FastAPI()
 
-            app.get("/disciplines")(self.disciplines)
-            app.post("/predict_disciplines")(self.predict_disciplines)
-            app.post("/update_model")(self.update_model)
+            app.get(
+                "/disciplines",
+                summary="The list of all supported disciplines.",
+                description="""
+                
+                See <https://vocabs.openeduhub.de/w3id.org/openeduhub/vocabs/discipline/index.html> for a list of all disciplines
+                """,
+            )(self.disciplines)
+            app.post(
+                "/predict_disciplines",
+                summary="Predict the disciplines belonging to the given text.",
+                description="""
+                Note that all disciplines will be returned, sorted by their
+                median predicted probability of being relevant to the text.
+                
+                Parameters
+                ----------
+                text : str
+                    The text to be analyzed.
+                num_samples : int
+                    The number of samples to use in order to estimate the
+                    fit of each discipline.
+                    Higher numbers will result in less variance between calls,
+                    but take more time.
+                interval_size : float (0, 1]
+                    The size of the credibility interval for the probability
+                    that a discipline is assigned to the given text.
+                    E.g. at 0.8, there is a probability of 80% that the
+                    predicted probability of the discipline belonging to the
+                    text is within the returned interval.
+
+                Returns
+                -------
+                disciplines : list of Discipline
+                    The list of disciplines, see below.
+                version : str
+                    The version of the prediction tool.
+
+                Discipline
+                ----------
+                name : str
+                    The name of the discipline.
+                mean_prob : float [0, 1]
+                    The mean of the predicted probabilities that this discipline
+                    belongs to the given text.
+                median_prob : float [0, 1]
+                    The median of the above probabilities.
+                prob_interval : 2-tuple of floats in [0, 1]
+                    The credibility interval of the predicted probabilities above.
+                """,
+            )(self.predict_disciplines)
+            app.post(
+                "/update_model",
+                summary="Update the model with a text and discipline assignment.",
+                description="""
+                Note that the given assignment of disciplines for the text is
+                assumed to be *fully* correct. This means that all listed
+                disciplines are actually relevant to the text and that *all
+                of the unlisted disciplines are not relevant*.
+                After the update, the model will predict higher probabilities
+                for the listed disciplines and lower probabilities for all
+                other disciplines.
+
+                Parameters
+                ----------
+                text : str
+                    The text based on which the model is to be updated.
+                classification : array of str
+                    The school disciplines that fit to the given text.
+                learning_rate : float (0, 1]
+                    The speed with which to learn from the data.
+                    The lower num_repeats is chosen, the lower this should be.
+                gamma : float (0, 1]
+                    The rate with which the learning rate will decrease
+                    during training. The learning rate for the last iteration
+                    will be gamma * learning_rate.
+                num_repeats : int > 0
+                    The number of times this text will be 'copied' for the update.
+                    Higher values will result in stronger changes in the updated
+                    model, but may negatively impact the performance on
+                    different texts.
+                    If the prediction on the text was relatively close to the
+                    desired results, choose a lower number. If it is very far
+                    off (e.g. almost 0), a higher number like 50 - 100 may be
+                    necessary.
+                num_train_iterations : int > 0
+                    The number of iterations to run the updating process for.
+                    Higher values will result in more consistent updates,
+                    but will also take a longer amount of time.
+                    Note that the update may be stopped earlier if
+                    convergence has been detected. The update will always run
+                    for at least 0.1 * num_train_iterations steps.
+                num_losses_head : int > 0
+                    The number of initial losses of the update to return.
+                num_losses_tail : in > 0
+                    The number of final losses of the update to return.
+
+                Returns
+                -------
+                losses_head : list of float
+                    The first few losses of the update.
+                losses_tail : list of float
+                    The last few losses of the update.
+                    If these values are close to the initial ones,
+                    you may need to increase the number of training iterations
+                    or the learning rate.
+                num_train_iterations : int
+                    The number of iterations that the update process was
+                    actually run for.
+                    If the first and final losses are similar, and this
+                    number is lower than the set number of training iterations,
+                    the learning rate is very likely to be too low.
+                """,
+            )(self.update_model)
 
             return app
 
