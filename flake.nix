627d16471bed85ad43fe2f03a68814c69eee970b
jokatzke <jokatzke@fastmail.com>
2024-04-05 15:50:31 +0200

    refactor: provide python library as overlay as well

--- flake.nix
+++ flake.nix
@@ -50,101 +50,130 @@
       flake-utils,
       ...
     }:
+    let
+      # some utility libraries
+      nix-filter = self.inputs.nix-filter.lib;
+
+      # lists of python packages required to run the application, build the
+      # model, or for development
+      python-packages = import ./python-packages.nix;
+
+      get-src =
+        excludeOptuna:
+        nix-filter {
+          root = self;
+          # only include files that are related to the application.
+          # this will prevent unnecessary rebuilds
+          include = [
+            (nix-filter.inDirectory ./its_jointprobability)
+            ./setup.py
+            ./requirements.txt
+          ];
+          exclude =
+            [ (nix-filter.matchExt "pyc") ]
+            # optionally ignore hyperparameter-optimization related code
+            ++ (nixpkgs.lib.lists.optionals excludeOptuna [
+              (nix-filter.inDirectory ./its_jointprobability/optuna)
+            ]);
+        };
+
+      # the python package containing the deployable service
+      get-python-lib-deploy =
+        pkgs: py-pkgs:
+        py-pkgs.buildPythonPackage {
+          pname = "its-jointprobability";
+          version = "0.2.1";
+          src = get-src true;
+          # replace local lookups of the model with the model that we pulled in
+          # the inputs
+          prePatch = ''
+            substituteInPlace its_jointprobability/*.py \
+              --replace "Path.cwd() / \"data\"" \
+                        "Path(\"${pkgs.its-jointprobability-model}\")"
+          '';
+          propagatedBuildInputs = (python-packages.deploy-pkgs py-pkgs);
+        };
+
+      # the python package also containing the dependencies for hyperparameter
+      # optimization
+      get-python-lib-optuna =
+        pkgs: py-pkgs:
+        (get-python-lib-deploy pkgs py-pkgs).overrideAttrs (oldAttrs: {
+          # no longer ignore the optuna module
+          src = get-src false;
+          # override the dependencies to include optuna-related packages and
+          # sqlite
+          propagatedBuildInputs = [ pkgs.sqlite ] ++ (python-packages.optuna-pkgs py-pkgs);
+        });
+    in
     {
-      # define an overlay to add the service to nixpkgs
-      overlays.default = (
-        final: prev: { its-jointprobability = self.packages.${final.system}.webservice; }
-      );
+      # define overlays to add the service or its library to nixpkgs
+      overlays = rec {
+        # just the webservice, as a native application
+        default = webservice;
+        webservice = (final: prev: { its-jointprobability = self.packages.${final.system}.webservice; });
+        # the python library, with and without optuna set up
+        python-lib = python-lib-deploy;
+        python-lib-deploy = nixpkgs.lib.composeManyExtensions [
+          # add dependencies
+          self.inputs.its-data.overlays.default
+          self.inputs.model.overlays.default
+          # define the actual overlay
+          (
+            final: prev:
+            let
+              get-python-lib = get-python-lib-deploy prev;
+            in
+            {
+              pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+                (python-final: python-prev: { its-jointprobability = get-python-lib python-prev; })
+              ];
+            }
+          )
+        ];
+        python-lib-optuna = nixpkgs.lib.composeManyExtensions [
+          # override the overlay without optuna
+          python-lib-deploy
+          (
+            final: prev:
+            let
+              get-python-lib = get-python-lib-optuna prev;
+            in
+            {
+              pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+                (python-final: python-prev: { its-jointprobability = get-python-lib python-prev; })
+              ];
+            }
+          )
+        ];
+      };
     }
     // flake-utils.lib.eachDefaultSystem (
       system:
       let
-        # import nixpkgs, optionally with CUDA support
-        pkgs-with-cuda = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            cudaSupport = true;
+        get-pkgs =
+          {
+            cudaSupport,
+            withOptuna ? false,
+          }:
+          import nixpkgs {
+            inherit system;
+            config = {
+              inherit cudaSupport;
+              allowUnfree = true;
+            };
+            overlays = [
+              (
+                if withOptuna then
+                  self.outputs.overlays.python-lib-optuna
+                else
+                  self.outputs.overlays.python-lib-deploy
+              )
+            ];
           };
-          overlays = [ self.inputs.its-data.overlays.default ];
-        };
-        pkgs-without-cuda = nixpkgs.legacyPackages.${system}.extend self.inputs.its-data.overlays.default;
-
-        # some utility libraries
-        openapi-checks = self.inputs.openapi-checks.lib.${system};
-        nix-filter = self.inputs.nix-filter.lib;
 
         # use the default python version (3.11 for nixos 23.11 and 24.05)
         get-python = pkgs: pkgs.python3;
-
-        # lists of python packages required to run the application, build the
-        # model, or for development
-        python-packages = import ./python-packages.nix;
-
-        model = self.inputs.model.packages.${system}.its-jointprobability-model;
-
-        get-src =
-          excludeOptuna:
-          nix-filter {
-            root = self;
-            # only include files that are related to the application.
-            # this will prevent unnecessary rebuilds
-            include = [
-              (nix-filter.inDirectory ./its_jointprobability)
-              ./setup.py
-              ./requirements.txt
-            ];
-            exclude =
-              [ (nix-filter.matchExt "pyc") ]
-              # optionally ignore hyperparameter-optimization related code
-              ++ (nixpkgs.lib.lists.optionals excludeOptuna [
-                (nix-filter.inDirectory ./its_jointprobability/optuna)
-              ]);
-          };
-
-        # the python package containing the deployable service
-        get-python-lib-deploy =
-          py-pkgs:
-          py-pkgs.buildPythonPackage {
-            pname = "its-jointprobability";
-            version = "0.2.1";
-            src = get-src true;
-            # replace local lookups of the model with the model that we pulled in
-            # the inputs
-            prePatch = ''
-              substituteInPlace its_jointprobability/*.py \
-                --replace "Path.cwd() / \"data\"" \
-                          "Path(\"${model}\")"
-            '';
-            propagatedBuildInputs = (python-packages.deploy-pkgs py-pkgs);
-          };
-
-        # the python package also containing the dependencies for hyperparameter
-        # optimization
-        get-python-lib-optuna =
-          pkgs: py-pkgs:
-          (get-python-lib-deploy py-pkgs).overrideAttrs (oldAttrs: {
-            # no longer ignore the optuna module
-            src = get-src false;
-            # override the dependencies to include optuna-related packages and
-            # sqlite
-            propagatedBuildInputs = [ pkgs.sqlite ] ++ (python-packages.optuna-pkgs py-pkgs);
-          });
-
-        # some simple wrappers for more easily creating the python applications
-        get-python-app-deploy =
-          pkgs:
-          let
-            py-pkgs = (get-python pkgs).pkgs;
-          in
-          py-pkgs.toPythonApplication (get-python-lib-deploy py-pkgs);
-
-        get-python-app-optuna =
-          pkgs:
-          let
-            py-pkgs = (get-python pkgs).pkgs;
-          in
-          py-pkgs.toPythonApplication (get-python-lib-optuna pkgs py-pkgs);
 
         # define the development environment
         get-devShell =
@@ -160,20 +189,38 @@
               pkgs.nix-init
             ];
           };
+
+        get-service =
+          { cudaSupport, withOptuna }:
+          (get-python (get-pkgs {
+            inherit cudaSupport withOptuna;
+          })).pkgs.its-jointprobability;
       in
       {
         # packages that we can build
         packages =
           rec {
-            webservice = get-python-app-deploy pkgs-without-cuda;
-            optuna-env = get-python-app-optuna pkgs-without-cuda;
+            webservice = get-service {
+              cudaSupport = false;
+              withOptuna = false;
+            };
+            optuna-env = get-service {
+              cudaSupport = false;
+              withOptuna = true;
+            };
             default = webservice;
           }
           //
           # CUDA is only supported on x86_64 linux
           (nixpkgs.lib.optionalAttrs (system == "x86_64-linux") rec {
-            webservice-with-cuda = get-python-app-deploy pkgs-with-cuda;
-            optuna-env-with-cuda = get-python-app-optuna pkgs-with-cuda;
+            webservice-with-cuda = get-service {
+              cudaSupport = true;
+              withOptuna = false;
+            };
+            optuna-env-with-cuda = get-service {
+              cudaSupport = true;
+              withOptuna = true;
+            };
             with-cuda = webservice-with-cuda;
           });
 
@@ -205,12 +252,20 @@
         # the development environment
         devShells =
           {
-            default = (get-devShell pkgs-without-cuda);
+            default = (
+              get-devShell (get-pkgs {
+                cudaSupport = false;
+              })
+            );
           }
           //
           # CUDA is only supported on x86_64 linux
           (nixpkgs.lib.optionalAttrs (system == "x86_64-linux") {
-            with-cuda = (get-devShell pkgs-with-cuda);
+            with-cuda = (
+              get-devShell (get-pkgs {
+                cudaSupport = true;
+              })
+            );
           });
 
         # integration testing
@@ -220,14 +275,16 @@
             # only run the VM checks on linux systems
             (system == "x86_64-linux" || system == "aarch64-linux")
             {
-              openapi-check = (
-                openapi-checks.test-service {
+              openapi-check =
+                let
+                  openapi-checks = self.inputs.openapi-checks.lib.${system};
+                in
+                (openapi-checks.test-service {
                   service-bin = "${self.packages.${system}.webservice}/bin/its-jointprobability --debug";
                   service-port = 8080;
                   openapi-domain = "/openapi.json";
                   memory-size = 4 * 1024;
-                }
-              );
+                });
             }
           );
       }
